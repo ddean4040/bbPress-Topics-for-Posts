6acd88908dd44a65c542570cd64d7ed6a050afc0
ddean <ddean@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-09 23:18:44 +0000

    Tagging 0.5
    git-svn-id: http://plugins.svn.wordpress.org/bbpress-post-topics/tags/0.5@449313 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,328 @@
+<?php
+
+/*
+Plugin Name: bbPress Topics for Posts
+Plugin URI: http://www.generalthreat.com/projects/bbpress-post-topics
+Description: Give authors the option to replace the comments on a WordPress blog post with a topic from an integrated bbPress install
+Author: David Dean
+Version: 0.4
+Revision Date: 09/22/2011
+Requires at least: WP 3.0, bbPress 2.0-rc1
+Tested up to: WP 3.2.1 , bbPress 2.0
+Author URI: http://www.generalthreat.com/
+*/
+
+class BBP_PostTopics {
+	
+	/**
+	 * Add the bbPress topic option to the Discussion meta box
+	 */
+	function display_topic_option( $post ) {
+
+		if(!function_exists('bbp_has_forums')) {
+			?><br /><p><?php _e('bbPress Topics for Posts has been enabled, but cannot detect your bbPress setup.','bbpress-post-topics'); ?></p><?php
+			return;
+		}
+		
+		$bbpress_topic_defaults = get_option( 'bbpress_discussion_defaults', array('enabled' => false, 'forum_id' => 0, 'hide_topic' => false) );
+		
+		$bbpress_topic_default_status	  = ($bbpress_topic_defaults['enabled'] == 'on');
+		$bbpress_topic_default_forum	  = (int)$bbpress_topic_defaults['forum_id'];
+		$bbpress_topic_default_hide_topic = ($bbpress_topic_defaults['hide_topic'] == 'on');
+		
+		$bbpress_topic_status	= (get_post_meta( $post->ID, 'use_bbpress_discussion_topic', true) == 'true') || get_post_meta( $post->ID, 'use_bbpress_discussion_topic', true) || $bbpress_topic_default_status;
+		$bbpress_topic_hidden	= get_post_meta( $post->ID, 'bbpress_discussion_hide_topic', true) || $bbpress_topic_default_hide_topic;
+		$bbpress_topic_slug		= get_post_meta( $post->ID, 'bbpress_discussion_topic_id', true);
+		if($bbpress_topic_slug) {
+			$bbpress_topic = bbp_get_topic( $bbpress_topic_slug);
+			$bbpress_topic_slug = $bbpress_topic->post_name;
+		}
+		
+		add_filter( 'bbp_has_forums_query', 'bbppt_remove_parent_forum_restriction' );
+		$forums = bbp_has_forums();
+		
+		if(!$forums) {
+			?><br /><p><?php _e('bbPress Topics for Posts has been enabled, but you have not created any forums yet.','bbpress-post-topics'); ?></p><?php
+			return; 
+		} 
+		?>
+		<br />
+		<label for="bbpress_topic_status" class="selectit"><input name="bbpress_topic_status" type="checkbox" id="bbpress_topic_status" value="open" <?php checked($bbpress_topic_status); ?> /> <?php _e( 'Use a bbPress forum topic for comments on this post.', 'bbpress-post-topics' ); ?></label><br />
+		<div id="bbpress_topic_status_options" style="display: <?php echo checked($bbpress_topic_status, true, false) ? 'block' : 'none' ?>;">
+			 &mdash; <label for="bbpress_topic_slug"><?php _e('Use an existing topic:', 'bbpress-post-topics' ) ?> </label> <input type="text" name="bbpress_topic_slug" id="bbpress_topic_slug" value="<?php echo $bbpress_topic_slug ?>" />
+			  - OR - <label for="bbpress_topic_forum"><?php _e('Create a new topic in forum:', 'bbpress-post-topics' ); ?></label>
+			<select name="bbpress_topic_forum" id="bbpress_topic_forum">
+				<option value="0" selected><?php _e('Select a Forum', 'bbpress-post-topics' ); ?></option>
+				<?php while ( bbp_forums() ) : bbp_the_forum(); ?>
+					<?php if(bbp_is_forum_category())	continue; ?>
+					<option value="<?php echo bbp_get_forum_id() ?>" <?php selected($bbpress_topic_default_forum,bbp_get_forum_id()) ?>><?php if(bbp_get_forum_parent()) echo '&mdash; ' ?><?php echo bbp_get_forum_title(); ?></option>
+				<?php endwhile; ?>
+			</select><br />
+			&mdash; <input type="checkbox" <?php checked($bbpress_topic_hidden) ?> name="bbpress_topic_hidden" id="bbpress_topic_hidden" /> <label for="bbpress_topic_hidden"><?php _e('Show only replies on the post page','bbpress-post-topics'); ?></label>
+		</div>
+		<script type="text/javascript">
+
+			/** hide options when not checked */
+			jQuery('#bbpress_topic_status').change(function() {
+				if(jQuery(this).attr('checked')) {
+					jQuery('#bbpress_topic_status_options').show();
+				} else {
+					jQuery('#bbpress_topic_status_options').hide();
+				}
+			});
+			
+			/** disable topic slug field when a forum is selected to prevent confusion */
+			jQuery('#bbpress_topic_forum').change(function() {
+				if(jQuery(this).val() != 0) {
+					jQuery('#bbpress_topic_slug').attr('disabled','true');
+				} else {
+					jQuery('#bbpress_topic_slug').removeAttr('disabled');
+				}
+			});
+			
+		</script>
+		<?php
+	}
+	
+	/**
+	 * Process the user's bbPress topic selections when the post is saved
+	 */
+	function process_topic_option( $post_ID, $post ) {
+
+		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
+			return;
+
+		if( !in_array( $post->post_type, apply_filters( 'bbppt_eligible_post_types', array('post','page') ) ) ) {
+			return;			
+		}
+
+		/**
+		 * The user requested to use a bbPress topic for discussion
+		 */
+		if( isset($_POST['bbpress_topic_status']) && $_POST['bbpress_topic_status'] == 'open' ) {
+
+			if(!function_exists('bbp_has_forums')) {
+				?><br /><p><?php _e('bbPress Topics for Posts cannot process this request because it cannot detect your bbPress setup.','bbpress-post-topics'); ?></p><?php
+				return;
+			}
+
+			$topic_slug   = isset($_POST['bbpress_topic_slug']) ? $_POST['bbpress_topic_slug'] : '' ;
+			$topic_forum  = isset($_POST['bbpress_topic_forum']) ? (int)$_POST['bbpress_topic_forum'] : 0 ;
+			$topic_hidden = isset($_POST['bbpress_topic_hidden']) ? true : false ;
+			
+			if($topic_slug != '') {
+				/** if user has selected an existing topic */
+				
+				if(is_numeric($topic_slug)) {
+					$topic = bbp_get_topic( (int)$topic_slug );
+				} else {
+					$topic = bbppt_get_topic_by_slug( $topic_slug );
+				}
+				
+				if($topic == null) {
+					// return an error of some kind
+					die('there was an error selecting the existing topic');
+				} else {
+					$topic_ID = $topic->ID;
+					update_post_meta( $post_ID, 'use_bbpress_discussion_topic', true );
+					update_post_meta( $post_ID, 'bbpress_discussion_topic_id', $topic_ID );
+					update_post_meta( $post_ID, 'bbpress_discussion_hide_topic', $topic_hidden );
+				}
+				
+			} else if($topic_forum != 0) {
+				/** if user has opted to create a new topic */
+				
+				$topic_content = ($post->post_excerpt != '') ? apply_filters('the_excerpt', $post->post_excerpt) : bbppt_post_discussion_get_the_content($post->post_content, 25) ;
+				$topic_content .= "<br />" . sprintf( __('[See the full post at: <a href="%s">%s</a>]'), get_permalink( $post_ID), get_permalink( $post_ID) );
+				
+				$new_topic_data = array(
+					'post_parent'   => $topic_forum,
+					'post_author'   => $post->post_author,
+					'post_content'  => $topic_content,
+					'post_title'    => $post->post_title,
+				);
+				
+				$new_topic_meta = array(
+					'forum_id'			=> $topic_forum
+				);
+				
+				$new_topic = bbp_insert_topic( $new_topic_data, $new_topic_meta );
+				if(!$new_topic) {
+					// return an error of some kind
+					die('there was an error creating a new topic');
+				} else {
+					update_post_meta( $post_ID, 'use_bbpress_discussion_topic', true );
+					update_post_meta( $post_ID, 'bbpress_discussion_topic_id', $new_topic );
+					update_post_meta( $post_ID, 'bbpress_discussion_hide_topic', $topic_hidden );
+				}
+				
+			}
+		} else {
+			delete_post_meta( $post_ID, 'use_bbpress_discussion_topic' );
+			delete_post_meta( $post_ID, 'bbpress_discussion_topic_id' );
+			delete_post_meta( $post_ID, 'bbpress_discussion_hide_topic' );
+		}
+	}
+	
+	/**
+	 * Display the bbPress topic plugin template instead of the WordPress comments template
+	 */
+	function maybe_change_comments_template( $template ) {
+
+		global $post, $bbp;
+
+		if(!function_exists('bbp_has_forums'))	return $template;
+		
+		if(get_post_meta( $post->ID, 'use_bbpress_discussion_topic', true)) {
+			$topic_ID = get_post_meta( $post->ID, 'bbpress_discussion_topic_id', true); 
+			if(file_exists(dirname( __FILE__ ) . '/templates/' . 'comments-bbpress.php')) {
+				$bbp->topic_query->post->ID = $topic_ID;
+				
+				if(get_post_meta( $post->ID, 'bbpress_discussion_hide_topic', true)) {
+					add_filter( 'bbp_has_replies_query', 'bbppt_remove_topic_from_thread' );
+				}
+				
+		 		return dirname( __FILE__ ) . '/templates/' . 'comments-bbpress.php';
+			}
+		}
+		return $template;
+	}
+	
+	/**
+	 * If a topic has been used for a post, give the number of replies in place of comment count
+	 */
+	function maybe_change_comments_number( $number, $post_ID ) {
+		
+		if(!function_exists('bbp_has_forums'))	return $number;
+		
+		if(get_post_meta( $post_ID, 'use_bbpress_discussion_topic', true)) {
+			$topic_ID = get_post_meta( $post_ID, 'bbpress_discussion_topic_id', true);
+			return bbp_get_topic_reply_count( $topic_ID );
+		}
+		
+		return $number;
+	}
+	
+	
+	/****************************
+	 * General Discussion options
+	 */
+	function add_discussion_page_settings() {
+		register_setting( 'discussion', 'bbpress_discussion_defaults' );
+		add_settings_field( 'bbpress_discussion_defaults', __('bbPress Topics for Posts Defaults','bbpress-post-topics'), array(&$this,'general_discussion_settings'), 'discussion', 'default', array('label_for'=>'bbpress_discussion_defaults_enabled') );
+	}
+	
+	function general_discussion_settings() {
+		$ex_options = get_option( 'bbpress_discussion_defaults' );
+		
+		add_filter( 'bbp_has_forums_query', 'bbppt_remove_parent_forum_restriction' );
+		$forums = bbp_has_forums();
+	
+		$forum_options = array();
+		$forum_options[0] = __('Select a Forum','bbpress-post-topics');
+		while ( bbp_forums() ) : bbp_the_forum();
+			if(bbp_is_forum_category())	continue;
+			$forum_options[bbp_get_forum_id()] = (bbp_get_forum_parent() ? ' &mdash; ' : '') . bbp_get_forum_title();
+		endwhile;
+		
+		$forum_select_string = '<select name="bbpress_discussion_defaults[forum_id]" id="bbpress_discussion_defaults_forum_id">';
+		foreach($forum_options as $forum_ID => $forum_name) {
+			$forum_select_string .= '<option value="' . $forum_ID . '" ' . selected( $ex_options['forum_id'], $forum_ID, false ) . '>' . $forum_name . '</option>';
+		}
+		$forum_select_string .= '</select>';
+		
+		?>
+		<input type="checkbox" name="bbpress_discussion_defaults[enabled]" id="bbpress_discussion_defaults_enabled" <?php checked($ex_options['enabled'],'on') ?>>
+		<label for="bbpress_discussion_defaults_enabled"><?php printf(_('Create a new bbPress topic in %s %s for new posts','bbpress-post-topics'), '</label>', $forum_select_string); ?><br />
+		<input type="checkbox" name="bbpress_discussion_defaults[hide_topic]" id="bbpress_discussion_defaults_hide_topic" <?php checked($ex_options['hide_topic'],'on') ?> /><label for="bbpress_discussion_defaults_hide_topic"><?php _e('Show only replies on the post page','bbpress-post-topics'); ?></label>
+		<?php
+	}
+}
+
+$bbp_post_topics = new BBP_PostTopics;
+
+add_action( 'post_comment_status_meta_box-options', array(&$bbp_post_topics,'display_topic_option') );
+add_action( 'save_post', array(&$bbp_post_topics,'process_topic_option'), 10, 2 );
+add_action( 'admin_init', array(&$bbp_post_topics, 'add_discussion_page_settings') );
+add_filter( 'comments_template', array(&$bbp_post_topics,'maybe_change_comments_template') );
+add_filter( 'get_comments_number', array(&$bbp_post_topics,'maybe_change_comments_number'), 10, 2 );
+
+/****************************
+ * Utility functions
+ */
+
+/**
+ * Check for a bbPress topic with a post_name matching the slug provided
+ * @param string Post slug
+ * @return object|NULL the topic or NULL if not found
+ */
+function bbppt_get_topic_by_slug( $slug ) {
+	
+	global $bbp, $wpdb;
+	
+	$topic = $wpdb->get_row( $wpdb->prepare('SELECT ID, post_name, post_parent FROM ' . $wpdb->posts .  ' WHERE post_name = %s AND post_type = %s', $slug, bbp_get_topic_post_type()) );
+	
+	if(is_null($topic))	return $topic;
+	return $topic;
+}
+
+/**
+ * Filter and limit the content for use in the bbPress topic
+ * @param text $content Post content to be filtered
+ * @param int $cut # of words to keep in the exceprt (set to 0 for whole post)
+ * @param int $encode_html flag to determine HTML tag processing - see the_content_rss() for details
+ * @return text filtered content
+ */
+function bbppt_post_discussion_get_the_content( $content, $cut = 0, $encode_html = 0 ) {
+	$content = apply_filters('the_content_rss', $content);
+	if ( $cut && !$encode_html )
+		$encode_html = 2;
+	if ( 1== $encode_html ) {
+		$content = esc_html($content);
+		$cut = 0;
+	} elseif ( 0 == $encode_html ) {
+		$content = make_url_footnote($content);
+	} elseif ( 2 == $encode_html ) {
+		$content = strip_tags($content);
+	}
+	if ( $cut ) {
+		$blah = explode(' ', $content);
+		if ( count($blah) > $cut ) {
+			$k = $cut;
+			$use_dotdotdot = 1;
+		} else {
+			$k = count($blah);
+			$use_dotdotdot = 0;
+		}
+
+		for ( $i=0; $i<$k; $i++ )
+			$excerpt .= $blah[$i].' ';
+		$excerpt .= ($use_dotdotdot) ? '...' : '';
+		$content = $excerpt;
+	}
+	$content = str_replace(']]>', ']]&gt;', $content);
+	return $content;
+}
+
+/**
+ * Remove the `post_parent` field from the forum query to get a list of all available forums
+ * Made for use with the bbp_has_forums_query filter
+ */
+function bbppt_remove_parent_forum_restriction( $bbp_args ) {
+	
+	$new_args = array();
+	foreach($bbp_args as $key => $arg) {
+		if($key != 'post_parent') {
+			$new_args[$key] = $arg;
+		}
+	}
+	
+	return $new_args;
+}
+
+function bbppt_remove_topic_from_thread( $bbp_args ) {
+	$bbp_args['post_type'] = bbp_get_reply_post_type();
+	return $bbp_args;
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,73 @@
+=== bbPress Topics for Posts ===
+Contributors: ddean
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=6BKDCMJRYPKNN&lc=US&item_name=bbPress%20Post%20Topics&currency_code=USD
+Tags: bbpress, topic, forum, post, page, comments, discussion
+Requires at least: 3.0
+Tested up to: 3.2.1
+Stable tag: 0.5
+
+Replace the comments on your WordPress blog posts with topics from an integrated bbPress install
+
+== Description ==
+
+Adds an option to the Discussion meta box to use a bbPress topic instead of WordPress comments, and displays that topic beneath the post on your site.
+
+You can let the plugin create a new topic for you in the forum of your choice, or specify an existing topic to attach to the post.
+A topic can be attached to as many posts as you'd like, but only one topic can currently be attached to a post.
+
+= Notes =
+
+Please do not use on production sites without testing first.
+
+== Installation ==
+
+1. Extract the plugin archive 
+1. Upload plugin files to your `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+= I don't see the Discussion meta box. Where is it? =
+
+See <a href="http://www.simplethemes.com/tutorials/help/disable-comments-in-wordpress">this article</a> for a quick tip on how to toggle the Discussion meta box.
+
+== Screenshots ==
+
+1. The post Discussion meta box
+2. Default post settings on the Discussion page
+
+== Changelog ==
+
+= 0.5 =
+* Added: can set creating a bbPress topic for discussion as default for new posts
+* Changed: post meta format
+
+= 0.4 =
+* Added: option to show only replies on the post / page
+* Fixed: link in forum topic was to nowhere - thanks, justin-mason
+* Fixed: could not select child forums when creating a new topic - thanks, justin-mason
+
+= 0.3 =
+* Added: show reply count instead of comment count for posts with an attached forum topic
+* Changed: display an error if bbPress is not detected instead of failing silently
+* Changed: how bbPress is detected
+
+= 0.2 =
+* Added: can use a topic ID or topic post_name/slug for existing topic
+* Added: better handling of bbPress environment
+* Fixed: use existing topic now works as expected
+* Fixed: finally settled on a name
+
+= 0.1 =
+Initial Release
+
+== Upgrade Notice ==
+
+= 0.5 =
+Added settings to Discussion page to make bbPress topics the default commenting option
+
+= 0.4 =
+Fixed a bug that put an empty link into forum topics. All users should upgrade.
+
+= 0.3 =
+Better error handling and improved integration
